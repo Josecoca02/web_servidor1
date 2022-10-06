727589689543553c2354950f53517d0a371c851f
Josecoca02 <115150610+Josecoca02@users.noreply.github.com>
2022-10-06 14:31:40 +0200

    commit inicial

---
+++ 02formularios/formulario1_respuesta.php
@@ -0,0 +1,13 @@
+<h1>Respuesta al formulario</h1>
+<?php
+    $nombre= $_POST["nombre"];
+    $edad= $_POST["edad"];
+
+    echo "<p>$nombre</p>";
+    echo "<p>$edad</p>";
+
+    /*CREAR FORMULARIO QUE RECIBA UN NUMERO
+    HABRA QUE CREAR UNA LISTA CON TANTOS NUMEROS COMO SE HAYA INDICADO */
+
+    
+?>
