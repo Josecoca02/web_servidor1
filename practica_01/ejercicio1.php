eabd596961e6a80cd87df52d18d64d308f168959
Josecoca02 <Josecoca0213@gmail.com>
2022-10-13 21:02:12 +0200

    commit de por la tarde

---
+++ practica_01/ejercicio1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EJercicio5</title>
+</head>
+<body>
+    <form action="respuesta1.php" method="post">
+        <label>Numero1 </label><br>
+        <input type="text" name="numeroa"><br><br>
+        <label>Numero2</label><br>
+        <input type="text" name="numerob"><br><br>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
---
+++ practica_01/respuesta1.php
@@ -0,0 +1,6 @@
+<?php
+    $numeroa =$_POST["numeroa"];
+    $numerob =$_POST["numerob"];
+    $contador =2;
+    echo "$numeroa y el $numerob";
+?>
