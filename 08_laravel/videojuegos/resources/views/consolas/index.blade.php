d0eb750f3cec59ebe850c9b537132baaaf00ca7f
Josecoca02 <115150610+Josecoca02@users.noreply.github.com>
2022-11-24 14:13:05 +0100

    vamos mejoranod
    vamos

---
+++ 08_laravel/videojuegos/resources/views/consolas/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Consolas</title>
+</head>
+<body>
+    <h1>Index de consolas</h1>
+
+    <p>{{ $mensaje }}</p>
+
+    <ul>
+        @foreach($consolas as $consola)
+            <li>{{ $consola }}</li>
+        @endforeach
+    </ul>
+
+    @php
+        $hola = "Hola mundo";
+    @endphp
+
+    <p>{{ $hola }}</p>
+</body>
+</html>
